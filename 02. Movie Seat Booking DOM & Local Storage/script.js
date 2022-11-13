d727f08ddb970e1ef2e4194519d54ccad559a54e
srdjangojkov <srdjaliman@gmail.com>
2022-11-13 23:35:57 +0100

    Finish project 2

---
+++ 02. Movie Seat Booking DOM & Local Storage/script.js
@@ -0,0 +1,57 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.container .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+let ticketPrice = Number(movieSelect.value);
+
+populateUI();
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.container .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+movieSelect.addEventListener('change', e => {
+  ticketPrice = Number(e.target.value);
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+})
+
+container.addEventListener('click', e => {
+  if (e.target.classList.contains('seat') &&
+  !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+});
+
+updateSelectedCount();
